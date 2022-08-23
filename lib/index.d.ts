b20f30886e05a0713e504dc21bc1d9a558204770
Proladon <Proladon@gmail.com>
2022-08-23 14:33:48 +0800

    0.0.13

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -17,7 +17,18 @@ declare module '@shelter-zone/sz-express-utils/lib/logger' {
 declare module '@shelter-zone/sz-express-utils/lib/middleware/validate' {
   import { Request, Response, NextFunction } from 'express';
   import { ObjectSchema } from 'joi';
+  /**
+   * 驗證請求攜帶的資料是否合法
+   * @param model: joi model
+   * @param dataFrom: 'query' | 'body'
+   */
   export const requestDataValidate: (model: ObjectSchema, dataFrom: 'query' | 'body') => (req: Request, res: Response, next: NextFunction) => Promise<void>;
+  /**
+   * 驗證請求的使用者的 userToken 是否合法
+   * @param authDB: deta base
+   * @param PRIVATEKEY: string
+   */
+  export const useApiAuthentication: (authDB: any, PRIVATEKEY: string) => (req: Request, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
 
 }
 declare module '@shelter-zone/sz-express-utils' {
