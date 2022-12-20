52c348a6c5b11e4bb0538bf816f9521ee20b671a
Xzihnago <xzihnago@gmail.com>
2022-12-20 17:04:11 +0800

    fix: revert axios 1.2.1 -> 1.1.3

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -10,7 +10,6 @@ export const validateUserID = async (oAuthToken: string | undefined, dataUserId:
     method: 'get',
     url: 'https://discord.com/api/users/@me',
     headers: {
-      "Accept-Encoding": "gzip,deflate,compress",
       authorization: oAuthToken.startsWith('Bearer ') ? oAuthToken : `Bearer ${oAuthToken}`,
     },
   })
