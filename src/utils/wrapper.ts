8521baf780aef968aac40c161d3bc77061cdd82b
Proladon <Proladon@gmail.com>
2023-02-19 15:22:30 +0800

    add serviceWrapper

---
+++ src/utils/wrapper.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+
+export const serviceWrapper = async (
+  req: Request,
+  res: Response,
+  serviceHandler: CallableFunction,
+  options: { logger: CallableFunction },
+) => {
+  try {
+    await serviceHandler(req, res)
+  } catch (error: any) {
+    if (options.logger) await options.logger()
+    else console.log(error)
+    res.fail({ status: error.status, code: error.code, message: error.message })
+  }
+}
