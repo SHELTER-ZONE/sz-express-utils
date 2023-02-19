9c0231dd4a7dc790bf7808cdb90749f395cf0331
Proladon <Proladon@gmail.com>
2023-02-19 15:34:22 +0800

    serviceWrapper - optional options

--- src/utils/wrapper.ts
+++ src/utils/wrapper.ts
@@ -4,12 +4,12 @@ export const serviceWrapper = async (
   req: Request,
   res: Response,
   serviceHandler: CallableFunction,
-  options: { logger?: CallableFunction } = {},
+  options?: { logger?: CallableFunction },
 ) => {
   try {
     await serviceHandler(req, res)
   } catch (error: any) {
-    if (options.logger) await options.logger()
+    if (options?.logger) await options.logger()
     else console.log(error)
     res.fail({ status: error.status, code: error.code, message: error.message })
   }
